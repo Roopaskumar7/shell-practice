9f592a9d69035b818e145bea0e511583b84f7c8a
Roopaskumar7 <roopaskumar7@gmail.com>
2026-06-11 19:43:49 +0530

    special vars

--- special.sh
+++ special.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+echo "script name: $0"
+echo "current directory: $PWD"
+echo "user running this script: $USER"
+echo "home directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in background: $!"
 
-echo  "All variables passed to the script" $@"
+
+
+
